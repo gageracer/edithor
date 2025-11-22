4098f13d37b4589be8a8a919c7cefdbcdef479ee
gageracer <gageracer@gmail.com>
2025-11-21 20:58:37 -0800

    adaptor to vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,12 @@
-import { mdsvex } from "mdsvex";
-import adapter from "@sveltejs/adapter-auto";
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import { mdsvex } from 'mdsvex';
+import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: [vitePreprocess(), mdsvex()],
-	extensions: [".svelte", ".svx"],
-	kit: {
-		adapter: adapter(),
-	},
+	extensions: ['.svelte', '.svx'],
+	kit: { adapter: adapter() }
 };
 
 export default config;
